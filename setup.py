da3d2a8d2c0c7fe3caea6092fea39d4c6ea04145
bridgerdanby <bridger.danby@outlook.com>
2024-04-22 18:44:53 -0600

    adding these back in

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("C:/Users/bridg/Desktop/Sandbox/Rooster/rooster-deepface/README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("C:/Users/bridg/Desktop/Sandbox/Rooster/rooster-deepface/requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().split("\n")
+
+setuptools.setup(
+    name="deepface",
+    version="0.0.80",
+    author="Sefik Ilkin Serengil",
+    author_email="serengil@gmail.com",
+    description="A Lightweight Face Recognition and Facial Attribute Analysis Framework (Age, Gender, Emotion, Race) for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/serengil/deepface",
+    packages=setuptools.find_packages(),
+        package_data={
+        'deepface': ['/models/*.keras'],  # Include .h5 files in the models directory
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": ["deepface = deepface.DeepFace:cli"],
+    },
+    python_requires=">=3.5.5",
+    install_requires=requirements,
+)
